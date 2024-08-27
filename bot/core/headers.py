2fcb9c1e8d88a504de5d0c53c232682fa0e70c6d
Gerfesto <hameilleb@gmail.com>
2024-08-27 20:39:48 +0300

    init

---
+++ bot/core/headers.py
@@ -0,0 +1,19 @@
+headers = {
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+    'Sec-Ch-Ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+    'Sec-Ch-Ua-Mobile': '0',
+    'Sec-Ch-Ua-Platform': '"Android"',
+    'Upgrade-Insecure-Requests': '0',
+    'Sec-Fetch-User': '0',
+    'Accept': 'application/json, text/plain, */*',
+    'Content-Type': 'application/json',
+    'Origin': 'https://cats-frontend.tgapps.store',
+    'Referer': 'https://cats-frontend.tgapps.store/',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Priority': 'u=1, i'
+}
